df5d92188793fdb4e870257b6385977e6aa9526d
Nathan Eloe <powerofazure@gmail.com>
2026-03-20 08:39:52 -0500

    Add pushing blinky lights

--- 8am/blinky/blinky.s
+++ 8am/blinky/blinky.s
@@ -9,6 +9,7 @@
 gpio_oe:    .word   0xd0000024
 gpio_on:    .word   0xd0000014
 gpio_off:   .word   0xd0000018
+gpio_in:    .word   0xd0000004
 delay:  .word   1000
 
 .text
@@ -16,9 +17,15 @@ delay:  .word   1000
 main:
     movs    r0, #LED @ #26
     bl gpio_init
-    movs    r0, #1
+    movs    r0, #27
+    bl gpio_init
+    movs    r0, #4
+    bl gpio_init
+    movs    r0, #3
     lsl r0, #LED
     ldr r1, =gpio_oe
+    movs    r0, #1
+    lsl r0, #LED
     ldr r1, [r1]
     str r0, [r1]
     ldr r1, =gpio_on
@@ -32,3 +39,20 @@ main:
     ldr r1, =gpio_off
     ldr r1, [r1]
     str r0, [r1]
+loop:
+    ldr r1, =gpio_in
+    ldr r1, [r1]
+    ldr r0, [r1]
+    lsr r0, #4
+    lsl r0, #LED
+    ldr r1, =gpio_on
+    ldr r1, [r1]
+    str r0, [r1]
+    lsr r0, #LED
+    mov r1, #1
+    eor r0, r1
+    lsl r0, #LED
+    ldr r1, =gpio_off
+    ldr r1, [r1]
+    str r0, [r1]
+    b loop
